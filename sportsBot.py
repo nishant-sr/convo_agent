f9b16b00cbb33009a0d08e5c16c333ce375608d6
nishant-sr <nishant.srinivasan236@gmail.com>
2022-02-28 17:57:39 -0800

    baseball

--- sportsBot.py
+++ sportsBot.py
@@ -33,6 +33,15 @@ hockey_talk = ['What do you think of hockey?',
                'How thick is the ice?', 'The ice in a hockey arena is approximately 3/4 of an inch', 'What gear do hockey players wear?', '']
 
 
+
+
+baseball_talk = ['What do you think of baseball?','Baseball is very fun, but can be a bit slow at times. What is your favorite team?','The Blue Jays are awesome! Do you have a favorite player in the MLB?',
+                'I think Guerrero and Semien did really well last seaon.', 'How do you score a point in Baseball?','You score a point by getting a home-run!', 'What is a home-run?','A home-run is when a player goes around the diamond and makes it past the home base safely.',
+                'How many players can play baseball?' , 'At any given time, there are 9 players from the defense team and 1 player up for batting to make a total of 10 players' , 'What do you do when you are on defense?',
+                'When you are on defense, your goal is to prevent the other team from getting a homerun by working with your teammates','what is the fastest pitch ever?','the fastest pitch ever is 105.1 mph','who won the 2021 world series?',
+                'The Atlanta Braves won the 2021 world series','What is a foul?','Simply put, a foul is when the ball goes out of bounds or was not pitched properly','What is a strike?',
+                'A strike is when a batter swings and misses the ball in the strike zone','What kind of equipment do baseball players need when batting?','When batting, players need to wear a helmet, and use a baseball bat to hit the ball']
+
 # this allows the bot to learn responses
 list_trainer = ListTrainer(sports_bot)
 for item in (intial_talk, basketball_talk, hockey_talk):
